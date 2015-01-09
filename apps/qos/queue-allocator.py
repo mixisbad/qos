20613890f188fea4f63037763ea8ee2a2becad5b
mixisbad <pattanapoomy@gmail.com>
2015-01-09 01:19:52 -0800

    complete the Dijkstra part to check available bandwidth for each server on each outport - still need to merge with polling to let them work together

--- apps/qos/queue-allocator.py
+++ apps/qos/queue-allocator.py
@@ -2,47 +2,161 @@
 #  coding: utf-8
 
 import sys
+import Queue
 
 nodes = []
 server_nodes = []
 switch_nodes = []
 server = {}
 adjacent = []
+max_bandwidth = 3000000
+
+
+
+#dummy data for test
+poll_map = [
+[3000000, 3000000, 3000000, 3000000, 3000000], 
+[3000000, 3000000, 3000000, 3000000, 3000000], 
+[3000000, 3000000, 3000000, 3000000, 3000000], 
+[3000000, 3000000, 3000000, 3000000, 3000000], 
+[3000000, 3000000, 3000000, 3000000]]
+
+def get_bandwidth_on_link(switch,outport):
+    sw = poll_map[switch]
+    bandwidth = sw[outport]
+    return bandwidth
+
+def allocate_queue():
+    #assume all the bandwidth available in every link is 3000000 for now
+    index = 0
+    #loop through all switches
+    for focus_switch in adjacent:
+        #loop through all output port of a switch to set queues
+        port_num = 0        
+        for outport in focus_switch:
+            #if output port is another switch, begin to check available bandwidth for each server from this output port
+            if outport != -1 and outport < len(switch_nodes):
+
+                node_avai_bandw = [-1 for i in range(len(nodes))]
+                node_visited = [False for i in range(len(nodes))]
+
+                ban_node = index
+                 
+                #start using modified Dijkstra for bottle neck and 
+                #mark that don't come back to the switch whose outport itself which has the ref number as index
+                #this queue is suppose to contains only switch
+                process_queue = Queue.PriorityQueue()
+
+                #add a start node
+                #need to get from actual bandwidth map, node -> outport
+                bandwidth = get_bandwidth_on_link(index,port_num)
+                node_avai_bandw[outport] = bandwidth
+
+                #use max_bandwidth - bandwidth to be able to sort with priority queue
+                process_queue.put( (max_bandwidth - bandwidth, outport) )
+                node_visited[outport] = True
+
+                #start a modified Dijkstra
+                while not process_queue.empty():
+                    item = process_queue.get()
+                    node_id = item[1]
+                     
+                    #looking for available outgoing from the node
+                    ports = adjacent[node_id]
+                    count_port = 0
+                    for next_node in ports:
+                        if next_node == -1:
+                            count_port = count_port+1
+                            continue
+                        #if next node is not a ban node and isn't visited yet
+                        elif next_node != ban_node and not node_visited[next_node]:
+                            #need to get from actual bandwidth map, node -> outport
+                            bandwidth = get_bandwidth_on_link(node_id,count_port)
+                            bandwidth = min(bandwidth, node_avai_bandw[node_id])
+                            node_avai_bandw[next_node] = bandwidth
+                            node_visited[next_node] = True
+                            #if next node is a switch then add to queue for further processing
+                            if next_node < len(switch_nodes):
+                                process_queue.put( (max_bandwidth - bandwidth, next_node) )
+                            count_port = count_port + 1
+                        else:
+                            count_port = count_port + 1
+
+                #after get all the results of each server 
+
+                #calculate total bandwidth
+                total_bandwidth = 0
+                for i in range(len(switch_nodes),len(switch_nodes)+len(server_nodes)):
+                    if node_visited[i]:
+                        print nodes[i] + " : bw " + str(node_avai_bandw[i])
+                        total_bandwidth = total_bandwidth + node_avai_bandw[i]
+                print "total bandwidth for " + nodes[index] + "-eth" + str(port_num+1) + " : " + str(total_bandwidth)
+
+            port_num = port_num + 1
+        index = index + 1
+
+                 
 
 if __name__ == '__main__':
     topo_detail = open('topology_detail.txt','r')
     
-    line = topo_detail.readline()    
+    line = ""
+    item = line.split()
     #start mapping nodes
-    if line == 'node':
+    while len(item) == 0 or item[0] != 'node':
         line = topo_detail.readline()
-        while line != '':
-            item = line.split()
+        item = line.split()
 
-            #found server
-            if len(item) == 2:
-                server_nodes.append(item[0])
-                server[item[1]] = item[0]
 
-            #found switch
-            elif len(item) ==1:
-                switch_nodes.append(item[0])
+    line = topo_detail.readline()
+    item = line.split()
+    while len(item) > 0:
 
-            line = topo_detail.readline()
+        #found server
+        if len(item) == 2:
+            server_nodes.append(item[0])
+            server[item[1]] = item[0]
 
-        nodes = switch_nodes + server_nodes
+        #found switch
+        elif len(item) ==1:
+            switch_nodes.append(item[0])
 
-    line = topo_detail.realine()
-    #start building adjacent matrix
-    if line == 'adjacent':
         line = topo_detail.readline()
-        while line != '':
-            row = []
-            item = line.split()
-            for i in range(len(item)):
+        item = line.split()
+
+    nodes = switch_nodes + server_nodes
+        
+
+    
+    #start building adjacent matrix 
+    #adjacent information on tology_detail.txt contains only switches' adjacent information
+    #row of adjacent equal to number of switch
+
+    line = topo_detail.readline()
+    item = line.split()
+    while len(item) == 0 or item[0] != 'adjacent':
+        line = topo_detail.readline()
+        item = line.split()
+
+
+    line = topo_detail.readline()
+    item = line.split()
+    while len(item) > 0:
+        row = []
+        for i in range(len(item)):
+            if item[i] in nodes:
                 row.append( nodes.index(item[i]) )
-            adjacent.append( row )
-                
+            else:
+                row.append( -1 )
+        adjacent.append( row )
+        line = topo_detail.readline()
+        item = line.split()
+
+    
+    #call method to allocate queue on switch periodically
+    #it already has a map so it just has to get bandwidth information from polling
+    #assume all the bandwidth available in every link is 3000000 for now
+    #need to merge this code and polling.py together
             
-            
+    allocate_queue()
     
