27fce685e9aa27280443f4f48b48f49e0833aa23
mixisbad <pattanapoomy@gmail.com>
2015-01-11 14:30:28 -0800

    merge polling and queue allocator

--- apps/qos/queue-allocator.py
+++ apps/qos/queue-allocator.py
@@ -9,10 +9,17 @@ server_nodes = []
 switch_nodes = []
 server = {}
 switches = {}
+switches_dpid = {}
 adjacent = []
 max_bandwidth = 3000000
 
-
+linkbandwidth = 10.0
+traffic_file_name = "/home/mininet/floodlight-qos-beta-master/traffic.txt"
+switchnum = 0
+traffic_data = {}
+name_index = {}
+bandwidthout = [[]]
+f_ptr = 0
 
 #dummy data for test
 poll_map = [
@@ -22,8 +29,102 @@ poll_map = [
 [3000000, 3000000, 3000000, 3000000, 3000000], 
 [3000000, 3000000, 3000000, 3000000]]
 
+def build_port_name():
+    global name_index
+
+    page = urllib.request.urlopen('http://localhost:8080/wm/core/controller/switches/json')
+    line = page.read().decode("utf-8")
+
+    collections = json.loads(line)
+
+    switchnum = len(collections)
+    
+    for sw in collections:
+        dpid = sw["dpid"]
+        port_detail = {}
+        ports = sw["ports"]
+        for each_port in ports:
+            port_detail[each_port["portNumber"]] = each_port["name"]
+        name_index[dpid] = port_detail
+        
+
+def parsejson():
+    global traffic_data
+    global traffic_file_name
+    global name_index
+    global f_ptr 
+    global bandwidthout
+    global switchnum
+
+    page = urllib.request.urlopen('http://localhost:8080/wm/core/switch/all/flow/json')
+    
+    line = page.read().decode("utf-8")
+
+    f_ptr = open(traffic_file_name,'w',encoding='utf-8')
+    new_traffic_data = {}
+    switch_dicts = json.loads(line)
+    for switch_id in switch_dicts:
+        if switch_id in name_index:
+            switch_index = nodes.index(switches[switch_id])
+            for flow in switch_dicts[switch_id]:
+                match = flow["match"]
+                actions = flow["actions"]
+
+                for action in actions:
+                    if action["type"] == "OUTPUT":
+                        total_duration = 0
+                        total_byte = 0
+                        found = False
+                        if (switch_id,action["port"]) in traffic_data:
+                            temp_traffic = traffic_data[(switch_id,action["port"])]
+                            if str(match) in temp_traffic:
+                                found = True
+                                temp_flow = temp_traffic[str(match)]
+                                old_duration = temp_flow["duration"]
+                                old_bytecount = temp_flow["byteCount"]
+                                total_duration = (flow["durationSeconds"]+flow["durationNanoseconds"]/1000000000)-old_duration
+                                total_byte = flow["byteCount"]-old_bytecount
+                            
+                        if not found:
+                            total_duration = (flow["durationSeconds"]+flow["durationNanoseconds"]/1000000000)
+                            total_byte = flow["byteCount"]
+
+                        buildkey = (switch_id,action["port"])
+                        
+                        #add information into globall traffic data for next iteration 
+                        if buildkey not in new_traffic_data:
+                            new_traffic_data[buildkey] = {}
+                        new_traffic_data[buildkey][str(match)] = {}
+                        new_traffic_data[buildkey][str(match)]["duration"] = (flow["durationSeconds"]+flow["durationNanoseconds"]/1000000000)
+                        new_traffic_data[buildkey][str(match)]["byteCount"] = flow["byteCount"]
+                        
+                        bw = ((total_byte*8)/(total_duration))/1000000
+                        bandwidthout[switch_index][action["port"]-1][0] = bandwidthout[switch_index][action["port"]-1] - bw
+
+                        destination = match["networkDestination"]
+                        if destination in server:
+                            server_name = server[destination]
+                            server_index = server_nodes.index(server_name)+1
+                            bandwidthout[switch_index][action["port"]-1][server_index] = bandwidthout[switch_index][action["port"]-1][server_index] + bw
+                           
+    f_ptr.write(str(switchnum) + "\t" + str(switchnumport) + "\n")
+#if switch_id in name_index:
+#switch_index = nodes.index(switches[switch_id])
+    for key in name_index:
+        f_ptr.write(key + "\n" + nodes.index(key)+"\n")
+
+    for sw in range( len(bandwidthout) ):
+        for port in range ( len(sw) ):
+            f_ptr.write(str(bandwidthout[sw][port]) + " ")
+        f_ptr.write("\n")
+    print("-----------------------------------------------------")
+    f_ptr.closed
+    traffic_data = new_traffic_data
+                        
+
+
 def get_bandwidth_on_link(switch,outport):
-    sw = poll_map[switch]
+    sw = bandwidthout[switch]
     bandwidth = sw[outport]
     return bandwidth
 
@@ -102,6 +203,11 @@ if __name__ == '__main__':
     global nodes
     global server_nodes
     global switch_nodes
+    global bandwidthout
+    global max_bandwidth
+
+    build_port_name()
+
     topo_detail = open('topology_detail.txt','r')
     
     line = ""
@@ -125,11 +231,11 @@ if __name__ == '__main__':
         elif ":" in item[1]:
             switch_nodes.append(item[0])
             switches[item[1]] = item[0]
+            switches_dpid[item[0]] = item[1]
 
         line = topo_detail.readline()
         item = line.split()
 
-    switch_nodes.sort()
     nodes = switch_nodes + server_nodes
         
 
@@ -146,6 +252,7 @@ if __name__ == '__main__':
 
     adjacent = [[] for i in range len(switch_nodes)]
 
+
     line = topo_detail.readline()
     item = line.split()
     while len(item) > 0:
@@ -156,10 +263,24 @@ if __name__ == '__main__':
             else:
                 row.append( -1 )
         adjacent[nodes.index(item[0])] = row
+
         line = topo_detail.readline()
         item = line.split()
 
     
+
+    #begin the loop 
+
+    #initial banwidthout
+    bandwidthout = []
+    for adj_row in adjacent:
+        bw_row = [[] for i in range( len(adj_row)) ]
+        for i in range( len(bw_row) ):
+            row_info = [0 for j in range( len(server)+1 ) ]
+        bandwidthout.append(bw_row)
+    
+    parsejson()
+
     #call method to allocate queue on switch periodically
     #it already has a map so it just has to get bandwidth information from polling
     #assume all the bandwidth available in every link is 3000000 for now
@@ -167,3 +288,4 @@ if __name__ == '__main__':
             
     allocate_queue()
     
+    #end the loop
